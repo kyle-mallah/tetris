656d9882a872912376a141d5666e6b86bc1ba161
Kyle Mallah <kylemallah@hotmail.com>
2019-12-05 13:34:13 -0500

    adding tetromino_type to constants

--- src/scripts/constants/index.js
+++ src/scripts/constants/index.js
@@ -1,2 +1,13 @@
 export const GRID_WIDTH = 10;
 export const GRID_HEIGHT = 20;
+
+export const TETROMINO_TYPE = Object.freeze({
+    NONE:Symbol('NONE'),
+    I:Symbol('I'),
+    J:Symbol('J'),
+    L:Symbol('L'),
+    O:Symbol('O'),
+    S:Symbol('S'),
+    T:Symbol('T'),
+    Z:Symbol('Z')
+});
