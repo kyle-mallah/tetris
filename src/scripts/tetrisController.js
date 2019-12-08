47d0a64d42331f4e1dcbbad4e7e18acd78706dac
Kyle <kylemallah@hotmail.com>
2019-12-08 01:05:42 -0500

    Added isCollision() method to TetrisController and associated tests

--- src/scripts/tetrisController.js
+++ src/scripts/tetrisController.js
@@ -1,7 +1,28 @@
-// import Math;
 import { TETROMINO_TYPE, GRID_WIDTH, GRID_HEIGHT } from './constants';
+import { TETROMINO_LAYOUTS } from './constants/tetrominoLayouts';
 
 class TetrisController {
+
+    isCollision(board, tetromino) {
+        let [tetromino_offset_y, tetromino_offset_x] = tetromino.position;
+        let tetromino_layout = tetromino.getLayout();
+
+        for (let r = 0; r < 4; r++) {
+            for (let c = 0; c < 4; c++) {
+                if (r + tetromino_offset_y < GRID_HEIGHT && c + tetromino_offset_x < GRID_WIDTH) {
+
+                    let boardCell = board[r + tetromino_offset_y][c + tetromino_offset_x]
+                    let tetrominoCell = tetromino_layout[r][c];
+
+                    if (boardCell !== TETROMINO_TYPE.NONE && tetrominoCell === 1) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
 
     initBoard() {
         let board = [];
--- src/scripts/tetrisController.test.js
+++ src/scripts/tetrisController.test.js
@@ -1,11 +1,20 @@
-import tetrisController from './tetrisController';
-import { TETROMINO_TYPE, GRID_HEIGHT, GRID_WIDTH } from './constants'
+import TetrisController from './tetrisController';
+import FallingTetromino from './fallingTetromino';
+import {
+    TETROMINO_TYPE,
+    GRID_HEIGHT,
+    GRID_WIDTH,
+    TETROMINO_ROTATION_STATE
+} from './constants'
 
-let controller = new tetrisController();
+let controller = new TetrisController();
+
+let board;
+beforeEach(() => {
+    board = controller.initBoard();
+});
 
 test("Board has dimensions GRID_HEIGHT x GRID_WIDTH", () => {
-    let board = controller.initBoard();
-
     expect(board.length).toBe(GRID_HEIGHT);
 
     board.forEach((row) => {
@@ -15,12 +24,76 @@ test("Board has dimensions GRID_HEIGHT x GRID_WIDTH", () => {
 });
 
 test("Board is initialized with no tetrominos on it", () => {
-    let board = controller.initBoard();
-
     for (const row of board) {
         for (const cell of row) {
             expect(cell).toBe(TETROMINO_TYPE.NONE);
         }
     }
 
+});
+
+test("No Collision: FallingTetromino resting on bottom of empty board", () => {
+    board[GRID_HEIGHT-1] = Array(GRID_WIDTH).fill(TETROMINO_TYPE.NONE);
+    let fallingTetromino = new FallingTetromino(
+        TETROMINO_TYPE.O,
+        [GRID_HEIGHT-2, 0],
+        TETROMINO_ROTATION_STATE[0]);
+
+    expect(controller.isCollision(board, fallingTetromino)).toBeFalsy();
+});
+
+test("Collision: FallingTetromino overlapping non-empty bottom row of board", () => {
+    board[GRID_HEIGHT-1] = Array(GRID_WIDTH).fill(TETROMINO_TYPE.I);
+    let fallingTetromino = new FallingTetromino(
+        TETROMINO_TYPE.O,
+        [GRID_HEIGHT-2, 0],
+        TETROMINO_ROTATION_STATE[0]);
+
+    expect(controller.isCollision(board, fallingTetromino)).toBeTruthy();
+});
+
+test("No Collision: FallingTetromino sitting one row above non-empty bottom row of board", () => {
+    board[GRID_HEIGHT-1] = Array(GRID_WIDTH).fill(TETROMINO_TYPE.I);
+    let fallingTetromino = new FallingTetromino(
+        TETROMINO_TYPE.O,
+        [GRID_HEIGHT-3, 0],
+        TETROMINO_ROTATION_STATE[0]);
+
+    expect(controller.isCollision(board, fallingTetromino)).toBeFalsy();
+});
+
+test("No Collision: FallingTetromino sitting one row below non-empty bottom row of board", () => {
+    board[GRID_HEIGHT-3] = Array(GRID_WIDTH).fill(TETROMINO_TYPE.I);
+    let fallingTetromino = new FallingTetromino(
+        TETROMINO_TYPE.O,
+        [GRID_HEIGHT-2, 0],
+        TETROMINO_ROTATION_STATE[0]);
+
+    expect(controller.isCollision(board, fallingTetromino)).toBeFalsy();
+});
+
+test("No Collision: FallingTetromino sitting one column right of non-empty board cell", () => {
+    board[GRID_HEIGHT-1] = Array(GRID_WIDTH).fill(TETROMINO_TYPE.I);
+    board[GRID_HEIGHT-1][GRID_WIDTH-1] = TETROMINO_TYPE.NONE;
+    board[GRID_HEIGHT-1][GRID_WIDTH-2] = TETROMINO_TYPE.NONE;
+
+    let fallingTetromino = new FallingTetromino(
+        TETROMINO_TYPE.O,
+        [GRID_HEIGHT-2, GRID_WIDTH-2],
+        TETROMINO_ROTATION_STATE[0]);
+
+    expect(controller.isCollision(board, fallingTetromino)).toBeFalsy();
+});
+
+test("No Collision: FallingTetromino sitting one column left of non-empty board cell", () => {
+    board[GRID_HEIGHT-1] = Array(GRID_WIDTH).fill(TETROMINO_TYPE.I);
+    board[GRID_HEIGHT-1][0] = TETROMINO_TYPE.NONE;
+    board[GRID_HEIGHT-1][1] = TETROMINO_TYPE.NONE;
+
+    let fallingTetromino = new FallingTetromino(
+        TETROMINO_TYPE.O,
+        [GRID_HEIGHT-2, -1],
+        TETROMINO_ROTATION_STATE[0]);
+
+    expect(controller.isCollision(board, fallingTetromino)).toBeFalsy();
 });
