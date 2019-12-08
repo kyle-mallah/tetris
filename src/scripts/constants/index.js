12eab6e03d63adcc55bdc25b4f0defa9346d9915
Kyle <kylemallah@hotmail.com>
2019-12-08 00:43:36 -0500

    constants/index.js: Changed values of enumerations to be strings instaed of Symbols

--- src/scripts/constants/index.js
+++ src/scripts/constants/index.js
@@ -2,19 +2,19 @@ export const GRID_WIDTH = 10;
 export const GRID_HEIGHT = 20;
 
 export const TETROMINO_TYPE = Object.freeze({
-    NONE:Symbol('NONE'),
-    I:Symbol('I'),
-    J:Symbol('J'),
-    L:Symbol('L'),
-    O:Symbol('O'),
-    S:Symbol('S'),
-    T:Symbol('T'),
-    Z:Symbol('Z')
+    NONE:'NONE',
+    I:'I',
+    J:'J',
+    L:'L',
+    O:'O',
+    S:'S',
+    T:'T',
+    Z:'Z'
 });
 
 export const TETROMINO_ROTATION_STATE = Object.freeze({
-    0:Symbol('0'),
-    1:Symbol('1'),
-    2:Symbol('2'),
-    3:Symbol('3'),
+    0:'0',
+    1:'1',
+    2:'2',
+    3:'3',
 });
