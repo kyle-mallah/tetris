89629a95e4c8af98535c75cab48f9355a3a4e823
Kyle <kylemallah@hotmail.com>
2019-12-07 23:11:57 -0500

    Changed TetrominoLayouts.js to export one object for all tetrominos. Fixed tetromino typo throughout TetrominoLayouts.js

--- src/scripts/constants/tetrominoLayouts.js
+++ src/scripts/constants/tetrominoLayouts.js
@@ -1,153 +1,163 @@
-export const TETROMINO_LAYOUT_I = Object.freeze({
+const I = {
     TETROMINO_ROTATION_STATE[0]: [[0,0,0,0],
                                   [1,1,1,1],
                                   [0,0,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[1]: [[0,0,1,0],
+    TETROMINO_ROTATION_STATE[1]: [[0,0,1,0],
                                   [0,0,1,0],
                                   [0,0,1,0],
                                   [0,0,1,0]],
 
-    TETRONIMO_ROTATION_STATE[2]: [[0,0,0,0],
+    TETROMINO_ROTATION_STATE[2]: [[0,0,0,0],
                                   [0,0,0,0],
                                   [1,1,1,1],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[3]: [[0,1,0,0],
+    TETROMINO_ROTATION_STATE[3]: [[0,1,0,0],
                                   [0,1,0,0],
                                   [0,1,0,0],
                                   [0,1,0,0]],
-});
+};
 
-export const TETROMINO_LAYOUT_J = Object.freeze({
-    TETRONIMO_ROTATION_STATE[0]: [[1,0,0,0],
+const J = {
+    TETROMINO_ROTATION_STATE[0]: [[1,0,0,0],
                                   [1,1,1,0],
                                   [0,0,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[1]: [[0,1,1,0],
+    TETROMINO_ROTATION_STATE[1]: [[0,1,1,0],
                                   [0,1,0,0],
                                   [0,1,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[2]: [[0,0,0,0],
+    TETROMINO_ROTATION_STATE[2]: [[0,0,0,0],
                                   [1,1,1,0],
                                   [0,0,1,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[3]: [[0,1,0,0],
+    TETROMINO_ROTATION_STATE[3]: [[0,1,0,0],
                                   [0,1,0,0],
                                   [1,1,0,0],
                                   [0,0,0,0]],
-});
+};
 
-export const TETROMINO_LAYOUT_L = Object.freeze({
-    TETRONIMO_ROTATION_STATE[0]: [[0,0,1,0],
+const L = {
+    TETROMINO_ROTATION_STATE[0]: [[0,0,1,0],
                                   [1,1,1,0],
                                   [0,0,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[1]: [[0,1,0,0],
+    TETROMINO_ROTATION_STATE[1]: [[0,1,0,0],
                                   [0,1,0,0],
                                   [0,1,1,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[2]: [[0,0,0,0],
+    TETROMINO_ROTATION_STATE[2]: [[0,0,0,0],
                                   [1,1,1,0],
                                   [1,0,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[3]: [[1,1,0,0],
+    TETROMINO_ROTATION_STATE[3]: [[1,1,0,0],
                                   [0,1,0,0],
                                   [0,1,0,0],
                                   [0,0,0,0]],
-});
+};
 
-export const TETROMINO_LAYOUT_O = Object.freeze({
-    TETRONIMO_ROTATION_STATE[0]: [[0,1,1,0],
+const O = {
+    TETROMINO_ROTATION_STATE[0]: [[0,1,1,0],
                                   [0,1,1,0],
                                   [0,0,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[1]: [[0,1,1,0],
+    TETROMINO_ROTATION_STATE[1]: [[0,1,1,0],
                                   [0,1,1,0],
                                   [0,0,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[2]: [[0,1,1,0],
+    TETROMINO_ROTATION_STATE[2]: [[0,1,1,0],
                                   [0,1,1,0],
                                   [0,0,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[3]: [[0,1,1,0],
+    TETROMINO_ROTATION_STATE[3]: [[0,1,1,0],
                                   [0,1,1,0],
                                   [0,0,0,0],
                                   [0,0,0,0]],
-});
+};
 
-export const TETROMINO_LAYOUT_S = Object.freeze({
-    TETRONIMO_ROTATION_STATE[0]: [[0,1,1,0],
+const S = {
+    TETROMINO_ROTATION_STATE[0]: [[0,1,1,0],
                                   [1,1,0,0],
                                   [0,0,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[1]: [[0,1,0,0],
+    TETROMINO_ROTATION_STATE[1]: [[0,1,0,0],
                                   [0,1,1,0],
                                   [0,0,1,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[2]: [[0,0,0,0],
+    TETROMINO_ROTATION_STATE[2]: [[0,0,0,0],
                                   [0,1,1,0],
                                   [1,1,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[3]: [[1,0,0,0],
+    TETROMINO_ROTATION_STATE[3]: [[1,0,0,0],
                                   [1,1,0,0],
                                   [0,1,0,0],
                                   [0,0,0,0]],
-});
+};
 
-export const TETROMINO_LAYOUT_T = Object.freeze({
-    TETRONIMO_ROTATION_STATE[0]: [[0,1,0,0],
+const T = {
+    TETROMINO_ROTATION_STATE[0]: [[0,1,0,0],
                                   [1,1,1,0],
                                   [0,0,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[1]: [[0,1,0,0],
+    TETROMINO_ROTATION_STATE[1]: [[0,1,0,0],
                                   [0,1,1,0],
                                   [0,1,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[2]: [[0,0,0,0],
+    TETROMINO_ROTATION_STATE[2]: [[0,0,0,0],
                                   [1,1,1,0],
                                   [0,1,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[3]: [[0,1,0,0],
+    TETROMINO_ROTATION_STATE[3]: [[0,1,0,0],
                                   [1,1,0,0],
                                   [0,1,0,0],
                                   [0,0,0,0]],
-});
+};
 
-export const TETROMINO_LAYOUT_Z = Object.freeze({
-    TETRONIMO_ROTATION_STATE[0]: [[1,1,0,0],
+const Z = {
+    TETROMINO_ROTATION_STATE[0]: [[1,1,0,0],
                                   [0,1,1,0],
                                   [0,0,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[1]: [[0,0,1,0],
+    TETROMINO_ROTATION_STATE[1]: [[0,0,1,0],
                                   [0,1,1,0],
                                   [0,1,0,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[2]: [[0,0,0,0],
+    TETROMINO_ROTATION_STATE[2]: [[0,0,0,0],
                                   [1,1,0,0],
                                   [0,1,1,0],
                                   [0,0,0,0]],
 
-    TETRONIMO_ROTATION_STATE[3]: [[0,1,0,0],
+    TETROMINO_ROTATION_STATE[3]: [[0,1,0,0],
                                   [1,1,0,0],
                                   [1,0,0,0],
                                   [0,0,0,0]],
-});
+};
+
+export const TETROMINO_LAYOUTS = Object.freeze({
+    "I": I,
+    "J": J,
+    "L": L,
+    "O": O,
+    "S": S,
+    "T": T,
+    "Z": Z,
+};
