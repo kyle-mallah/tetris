c4ea6f9933f5ff52d7033b04a50adb561ac5fd52
Kyle <kylemallah@hotmail.com>
2019-12-08 00:44:47 -0500

    FallingTetromino now holds onto all possible layouts and provides a getLayout() method

--- src/scripts/fallingTetromino.js
+++ src/scripts/fallingTetromino.js
@@ -1,13 +1,21 @@
+import { TETROMINO_LAYOUTS } from './constants/tetrominoLayouts';
+
 /**
     Represents a tetromino that has spawned but has not yet stopped falling.
     The position of a falling tetromino refers to the top-left corner of a 4x4
-    box in which this tetromino is located.
+    box with respect to the game board.
 */
 class FallingTetromino {
     constructor(tetrominoType, position, rotationState) {
         this.tetrominoType = tetrominoType;
         this.position = position;
         this.rotationState = rotationState;
+
+        this._layouts = TETROMINO_LAYOUTS[tetrominoType];
+    }
+
+    getLayout() {
+        return this._layouts[this.rotationState];
     }
 
 }
