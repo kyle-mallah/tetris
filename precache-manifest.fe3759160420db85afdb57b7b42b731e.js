567b9a69bd74353ba5941b4ff49998179ca4f537
Kyle <kylemallah@hotmail.com>
2019-12-15 21:29:19 -0500

    Updates

---
+++ precache-manifest.fe3759160420db85afdb57b7b42b731e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46422f334ca88cfa93676f5c4b6c971",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "9503cb140f08a9f53687",
+    "url": "/tetris/static/css/main.ca1bcbfa.chunk.css"
+  },
+  {
+    "revision": "c79f44a3105f79d281d4",
+    "url": "/tetris/static/js/2.d5d775bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tetris/static/js/2.d5d775bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9503cb140f08a9f53687",
+    "url": "/tetris/static/js/main.b6470394.chunk.js"
+  },
+  {
+    "revision": "7299ff2b414667a6e0ff",
+    "url": "/tetris/static/js/runtime-main.831c2a87.js"
+  }
+]);
